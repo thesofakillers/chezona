ce23b8ff24bbdb78399152e2ebcdac74aab7ae30
Giulio Starace <giulio.starace@gmail.com>
2021-01-14 16:36:54 +0100

    more flexible layout

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "gatsby";
+import { useStaticQuery, graphql } from "gatsby";
 
 const ListLink = (props) => (
   <li style={{ display: `inline-block`, marginRight: `1rem` }}>
@@ -8,11 +9,20 @@ const ListLink = (props) => (
 );
 
 export default function Layout({ children }) {
+  const data = useStaticQuery(graphql`
+    query LayoutQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
   return (
     <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
       <header style={{ marginBottom: `1.5rem` }}>
         <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
-          <h3 style={{ display: `inline` }}>Che Zona?</h3>
+          <h3 style={{ display: `inline` }}>{data.site.siteMetadata.title}?</h3>
         </Link>
         <ul style={{ listStyle: `none`, float: `right` }}>
           <ListLink to="/">Home</ListLink>
