c5630d5181c7f7e5554eb16a4d03136ad08bfd97
Giulio Starace <giulio.starace@gmail.com>
2021-01-14 16:38:24 +0100

    redirect users to covidzone.info. abandoning project

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { Link } from "gatsby";
+import { Link as a } from "gatsby";
 import Layout from "../components/layout";
 
 export default function Home() {
   return (
     <Layout style={{ margin: `3rem auto`, maxWidth: 600 }}>
-      <h1>Le regioni per colore</h1>
-      <Link to="/info/">Informazioni</Link>
+      <h1>Sito archiviato</h1>
+      Recatevi a <a href="https://covidzone.info">covidzone.info</a>
     </Layout>
   );
 }
--- src/pages/info.jsx
+++ src/pages/info.jsx
@@ -5,7 +5,41 @@ export default function Info() {
   return (
     <Layout>
       <h1>Info</h1>
-      <p>Such wow. Very React.</p>
+      <p>
+        Con la pioggia di decreti COVID per la classificazione in zone colorate
+        del paese, mi ero fatto l'idea (forse un po' tardi) di creare un
+        dashboard dove sintetizzare in modo chiaro qual era il colore di ogni
+        regione e in quali giorni. Ho dunque ho iniziato a lavorare su questo
+        sito ma mi sono poi accorto che la soluzione già esiste su{" "}
+        <a
+          href="https://covidzone.info"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          covidzone.info
+        </a>
+        . Perciò ho abbandonato questo progetto. Visto il dominio abbastanza
+        facile da ricordare, ho tenuto il sito comunque aperto, reindirizzando
+        gli utenti su{" "}
+        <a
+          href="https://covidzone.info"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          covidzone.info
+        </a>
+        . <br />
+        Il codice ormai abbandonato e comunque incompleto lo trovate sul mio
+        account{" "}
+        <a
+          href="https://github.com/thesofakillers/chezona"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+        .
+      </p>
     </Layout>
   );
 }
