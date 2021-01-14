bc02153bdf2e71b42f89e55939502038dfe35f48
Giulio Starace <giulio.starace@gmail.com>
2021-01-14 10:31:30 +0100

    fix google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,24 @@
 module.exports = {
   siteMetadata: {
-    title: "CheZona",
+    title: "Che Zona",
     siteUrl: "https://www.chezona.it",
   },
   plugins: [
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
     "gatsby-plugin-sass",
+    "gatsby-transformer-json",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src/data/`,
+        path: `${__dirname}/src/data/`,
+      },
+    },
     {
       resolve: "gatsby-plugin-google-analytics",
       options: {
-        trackingId: "258354107",
+        trackingId: "UA-143281261-3",
       },
     },
     {
